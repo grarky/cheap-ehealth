d7234667be44a3e320a73dbe48f9d39be82100f4
grarky <grarky@gmail.com>
2016-05-16 18:47:36 +0200

    Initial commit

---
+++ cheap-ehealth-server/src/main/java/com/cheap/ehealth/CheapEhealthServerApplication.java
@@ -0,0 +1,12 @@
+package com.cheap.ehealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapEhealthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheapEhealthServerApplication.class, args);
+	}
+}
